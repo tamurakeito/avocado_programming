864193e3d6d00aac601aa338a92f838a567c6f9b
tamurakeito <kit_tamtam@icloud.com>
2025-05-23 20:12:50 +0900

    favicon設定

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,6 +25,22 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ja">
+      <head>
+        <link
+          rel="icon"
+          type="image/png"
+          href="/favicons/favicon-96x96.png"
+          sizes="96x96"
+        />
+        <link rel="icon" type="image/svg+xml" href="/favicons/favicon.svg" />
+        <link rel="shortcut icon" href="/favicons/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicons/apple-touch-icon.png"
+        />
+        <link rel="manifest" href="/favicons/site.webmanifest" />
+      </head>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <GeneralTemplate>{children}</GeneralTemplate>
       </body>
