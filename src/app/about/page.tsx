e6ecb9b52768c20b40943155c10065321d11ea3e
tamurakeito <kit_tamtam@icloud.com>
2025-05-24 15:26:30 +0900

    about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,26 @@
+import styles from "./styles.module.scss";
+
+const About = () => {
+  return (
+    <div className={styles.about}>
+      <div className={styles.about_heading}>
+        「アボカドプログラミング」へようこそ
+      </div>
+      <div className={styles.about_content}>
+        <div className={styles.about_content_text_container}>
+          <div className={styles.about_content_text}>
+            ここは、プログラミング経験が全くない方でも、「アプリ作ってみたいな！」というシンプルな好奇心から、気軽にアプリ開発の楽しさに触れられるサイトです。
+          </div>
+          <div className={styles.about_content_text}>
+            気軽にブログを読むように、わかりやすい記事を通して、あなたの「やってみたい！」という気持ちを応援し、アプリ開発に必要な基礎知識やフレームワークの使い方をお届けします。アボカドの豊かな栄養が、あなたの創造力を育み、新しいスキルを習得する助けになることを願っています🥑
+          </div>
+          <div className={styles.about_content_text}>
+            いずれは、皆さんのアイデアを本格的に形にするための学習プログラムやサポートも提供していきたいと考えています。まずは気軽に、ここからアプリ開発の世界を覗いてみてください。あなたの「作ってみたい！」が、きっとここから始まります。
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
