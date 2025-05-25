2b1fff1361d8c027a125de7f7a64987e5b333d3b
tamurakeito <kit_tamtam@icloud.com>
2025-05-25 16:17:52 +0900

    error_handling page

---
+++ src/app/error-handling/page.tsx
@@ -0,0 +1,19 @@
+import styles from "./styles.module.scss";
+import classNames from "classnames";
+
+const ErrorHandling = () => {
+  return (
+    <div className={styles.error_handling}>
+      <div className={styles.error_handling_heading}>
+        エラーでつまづいたら...
+      </div>
+      <div className={styles.error_handling_content}>
+        <div className={styles.error_handling_content_text}>プログラミングの学習中にエラーメッセージが表示されると、ドキッとしたり、どうすれば良いか分からなくなったりしますよね。でも焦らなくて大丈夫です。エラーは、プログラミングを学んでいれば誰でも必ず出会うものですし、決してあなたが悪い訳ではありません。</div>
+        <div className={classNames([styles.error_handling_content_text, styles.error_handling_content_text_bold])}>まずは、慌てずに深呼吸。</div>
+        <div className={styles.error_handling_content_text}>エラーは、プログラムが「ここがちょっと分からないよ」「こうしてくれると助かるんだけどな」と教えてくれているサインのようなものです。解決できたときには大きな達成感が得られますし、自分のスキルが一段とアップしていることに気づくはずです。</div>
+      </div>
+    </div>
+  );
+};
+
+export default ErrorHandling;
